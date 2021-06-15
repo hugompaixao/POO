3d79973bee53009b4d16986e2d1816b9e14c601d
hugompaixao <73443362+hugompaixao@users.noreply.github.com>
2021-06-15 23:20:06 +0100

    Update Point.java

--- Project1/Point.java
+++ Project1/Point.java
@@ -1,1 +1,15 @@
 
+public class Point {
+    private double x, y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p) {
+        double dx = x - p.x;
+        double dy = y - p.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+}
